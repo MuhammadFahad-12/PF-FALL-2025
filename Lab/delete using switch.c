d369dc2e23ae3ab134544380e45d2fa1422d66e3
MuhammadFahad-12 <faahadejaz98@gmail.com>
2025-10-12 20:41:30 +0500

    LAB tasks

---
+++ Lab/delete using switch.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    char choice;
+
+    printf("Are you sure you want to delete? [Y/N]: ");
+    scanf(" %c", &choice);
+
+    switch(choice) {
+        case 'Y':
+        case 'y':
+            printf("Deleted successfully.\n");
+            break;
+        case 'N':
+        case 'n':
+            printf("Delete cancelled.\n");
+            break;
+        default:
+            printf("Invalid choice! Please enter Y or N.\n");
+    }
+    return 0;
+}
