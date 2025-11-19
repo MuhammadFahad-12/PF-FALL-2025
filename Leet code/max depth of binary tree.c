a3bb42d1570211cfed6a97b166225216aafecd02
Muhammad Fahad <fahaadejaz98@gmail.com>
2025-11-20 00:24:56 +0500

    .

---
+++ Leet code/max depth of binary tree.c
@@ -0,0 +1,10 @@
+//https://leetcode.com/problems/maximum-depth-of-binary-tree/
+int maxDepth(struct TreeNode* root) {
+    if (root == NULL) 
+        return 0;
+
+    int leftDepth = maxDepth(root->left);
+    int rightDepth = maxDepth(root->right);
+
+    return (leftDepth > rightDepth ? leftDepth : rightDepth) + 1;
+}
